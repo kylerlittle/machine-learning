412755d32bb0d240ac1bf80d566a1d2e810a79fb
kylerlittle <kyler.little@wsu.edu>
2018-01-20 15:30:44 -0800

    bug free code

--- hw-1/code/main.py
+++ hw-1/code/main.py
@@ -1,6 +1,7 @@
 from helper import *
 from solution import *
 
+
 def play_with_data():
 	# show the data
 	traindataloc = "../data/train.txt"
@@ -10,33 +11,30 @@ def play_with_data():
 	w= math.floor(math.sqrt(d))
 	data = np.reshape(data, (nums, w, w))
 	show_images(data)
+	print("play with data done!")
 
 
 def play_with_features():
 	#get data
-	traindataloc, testdataloc = "../data/train.txt", "../data/test.txt"
-	train_data, train_label = load_features(traindataloc)
+	traindataloc,testdataloc = "../data/train.txt", "../data/test.txt"
+	train_data,train_label = load_features(traindataloc)
 	test_data, test_label = load_features(testdataloc)
-	show_features(train_data,train_label)
+	show_features(train_data[:,1:3],train_label)
+	print("play with features done!")
 
 
 def test_accuracy():
-	max_iter = [1, 3, 5, 10, 20]
+	max_iter = [10, 30, 50, 100, 200]
 	result_mi = [(0.973,0.948), (0.971, 0.950), (0.980, 0.962), (0.981, 0.959), (0.977, 0.948)]
-	learning_rate = [0.1, 0.2, 0.3, 0.5, 1.0]
+	learning_rate = [0.1, 0.2, 0.3, 0.4, 0.5]
 	result_lr = [(0.981, 0.962), (0.981, 0.962), (0.981, 0.962), (0.981, 0.962), (0.981, 0.962)]
 	for i, m_iter in enumerate(max_iter):
-		train_acc, test_acc = test_perceptron(m_iter, learning_rate[0], 1)
-		if equal(train_acc, result_mi[i][0]) and equal(test_acc, result_mi[i][1]):
-			print("testcase%d passed"% (i+1))
-		else:
-			print("testcase%d failed"% (i+1))
+		_, train_acc, test_acc = test_perceptron(m_iter, learning_rate[0])
+		print("Case %d train accuracy:%f  test accuracy: %f"%(i+1, train_acc, test_acc))
 	for i, l_rate in enumerate(learning_rate):
-		train_acc, test_acc = test_perceptron(max_iter[2], l_rate, 2)
-		if equal(train_acc, result_lr[i][0]) and equal(test_acc, result_lr[i][1]):
-			print("testcase%d passed"% (i+len(max_iter)+1))
-		else:
-			print("testcase%d failed"% (i+len(max_iter)+1))
+		_, train_acc, test_acc = test_perceptron(max_iter[4], l_rate)
+		print("Case %d train accuracy:%f  test accuracy: %f"%(i+6,train_acc, test_acc))
+	print("accuracy test done!")
 
 
 def play_with_result():
@@ -47,10 +45,15 @@ def play_with_result():
 	learning_rate = 0.5
 	w = perceptron(train_data, train_label, max_iter, learning_rate)	
 	show_result(test_data[:,1:3], test_label, w)
+	print("play with result done!")
 
 
 if __name__ == '__main__':
+	## test question (a)
 	play_with_data()
+	## test question (b)
 	play_with_features()
+	## test question (c)
 	test_accuracy()
+	# test question (d)
 	play_with_result()
--- hw-1/code/solution.py
+++ hw-1/code/solution.py
@@ -1,7 +1,5 @@
 import numpy as np 
 from helper import *
-import matplotlib.pyplot as plt
-from pylab import norm
 
 '''
 Homework1: perceptron classifier
@@ -12,123 +10,63 @@ def sign(x):
 #-------------- Implement your code Below -------------#
 
 def show_images(data):
-        '''
+	'''
 	This function is used for plot image and save it.
 
 	Args:
 	data: Two images from train data with shape (2, 16, 16). The shape represents total 2
-	images and each image has size 16 by 16. 
+	      images and each image has size 16 by 16. 
 
 	Returns:
-	Do not return any arguments, just save the images you plot for your report.
+		Do not return any arguments, just save the images you plot for your report.
 	'''
-        for index in range(len(data)):
-                imgplot = plt.imshow(data[index])
-                plt.show(imgplot)                  # After each figure pops up, I save it for the report
-
-
-                
-def assign_sym_col(label):
-        '''
-        This function assigns a red color & '*' symbol to labels '+1' (i.e. digit number 1) and a blue color & '+'
-        symbol to labels '-1' (i.e. digit number 5).
-
-        Args:
-        label: train data's label with shape (1561,1). 
-	1 for digit number 1 and -1 for digit number 5.
-
-        Returns:
-        A 2-tuple of two lists, each of length 1561
-        '''
-        symbols = []; colors = []
-        for val in label:
-                if val == 1:
-                        sym = '*'; col = 'red'
-                else:
-                        sym = '+'; col = 'blue'
-                symbols.append(sym); colors.append(col)
-        return (symbols, colors)
 
 
 def show_features(data, label):
-        '''
-        This function is used for plot a 2-D scatter plot of the features and save it.
-        
-        Args:
-        data: train features with shape (1561, 2). The shape represents total 1561 samples and 
-        each sample has 2 features.
-        label: train data's label with shape (1561,1). 
-        +1 for digit number 1 and -1 for digit number 5.
+	'''
+	This function is used for plot a 2-D scatter plot of the features and save it. 
 
-        Returns:
-        Do not return any arguments, just save the 2-D scatter plot of the features you plot for your report.
-        '''
-        fig, ax = plt.subplots()
-        (symbols, colors) = assign_sym_col(label)
-        for _s, c, _x, _y in zip(symbols, colors, data[:,1], data[:,2]):
-                ax.scatter(_x, _y, s=50, marker=_s, c=c)
-        plt.title('Training Data'); plt.xlabel('Symmetry Classifier'); plt.ylabel('Average Intensity')
-        plt.show()
+	Args:
+	data: train features with shape (1561, 2). The shape represents total 1561 samples and 
+	      each sample has 2 features.
+	label: train data's label with shape (1561,1). 
+		   1 for digit number 1 and -1 for digit number 5.
+	
+	Returns:
+	Do not return any arguments, just save the 2-D scatter plot of the features you plot for your report.
+	'''
 
-        
 
 def perceptron(data, label, max_iter, learning_rate):
-        '''
-        The perceptron classifier function.
-        
-        Args:
-        data: train data with shape (1561, 3), which means 1561 samples and 
-        each sample has 3 features.(1, symmetry, average internsity)
-        label: train data's label with shape (1561,1).
-        1 for digit number 1 and -1 for digit number 5.
-        max_iter: max iteration numbers
-        learning_rate: learning rate for weight update
+	'''
+	The perceptron classifier function.
 
-        Returns:
-        the seperater with shape (1, 3). You must initilize it with w = np.zeros((1,d))
-        '''
-        t = 0; w = np.zeros((1,len(data[0])))
-        while t < max_iter:
-                for data_index, data_val in enumerate(data):
-                        if sign(np.dot(w[0], data_val)) != label[data_index]:   # Misclassified Item
-                                w += learning_rate * label[data_index] * data_val  # Update weight
-                t += 1
-        return w
+	Args:
+	data: train data with shape (1561, 3), which means 1561 samples and 
+		  each sample has 3 features.(1, symmetry, average internsity)
+	label: train data's label with shape (1561,1). 
+		   1 for digit number 1 and -1 for digit number 5.
+	max_iter: max iteration numbers
+	learning_rate: learning rate for weight update
+	
+	Returns:
+		w: the seperater with shape (1, 3). You must initilize it with w = np.zeros((1,d))
+	'''
 
 
 def show_result(data, label, w):
-        '''
-        This function is used for plot the test data with the separators and save it.
-        
-        Args:
-        data: test features with shape (424, 2). The shape represents total 424 samples and
-        each sample has 2 features.
-        label: test data's label with shape (424,1).
-        +1 for digit number 1 and -1 for digit number 5.
-
-        Returns:
-        Do not return any arguments, just save the image you plot for your report.
-        '''
-        # Set up plot
-        fig, ax = plt.subplots()
-
-        # Overlay the linear separator
-        X = data[:,0]
-        Y = np.zeros(len(X))
-        line_slope = -w[0][1] / w[0][2]; y_intercept = -w[0][0] / w[0][2]
-        for index in range(len(Y)):
-                Y[index] = (line_slope * X[index]) + y_intercept
-        ax.plot(X, Y, color='k', linewidth=2)
-
-        # Add scatter plot
-        (symbols, colors) = assign_sym_col(label)
-        for _s, c, _x, _y in zip(symbols, colors, data[:,0], data[:,1]):
-                ax.scatter(_x, _y, s=50, marker=_s, c=c)
-
-        # Configure scaling, title, and xy labels
-        ax.set_xlim([X.min(), X.max()]); ax.set_ylim([-1., 0.2])
-        plt.title('Test Data'); plt.xlabel('Symmetry Classifier'); plt.ylabel('Average Intensity')
-        plt.show()
+	'''
+	This function is used for plot the test data with the separators and save it.
+	
+	Args:
+	data: test features with shape (424, 2). The shape represents total 424 samples and 
+	      each sample has 2 features.
+	label: test data's label with shape (424,1). 
+		   1 for digit number 1 and -1 for digit number 5.
+	
+	Returns:
+	Do not return any arguments, just save the image you plot for your report.
+	'''
 
 
 #-------------- Implement your code above ------------#
@@ -141,7 +79,7 @@ def accuracy_perceptron(data, label, w):
 	return (n-mistakes)/n
 
 
-def test_perceptron(max_iter, learning_rate, testcase, showresult = False):
+def test_perceptron(max_iter, learning_rate):
 	#get data
 	traindataloc,testdataloc = "../data/train.txt", "../data/test.txt"
 	train_data,train_label = load_features(traindataloc)
@@ -151,8 +89,6 @@ def test_perceptron(max_iter, learning_rate, testcase, showresult = False):
 	train_acc = accuracy_perceptron(train_data, train_label, w)	
 	#test perceptron model
 	test_acc = accuracy_perceptron(test_data, test_label, w)
-	if showresult:
-		show_result(test_data, test_label, w, testcase)
-	return train_acc, test_acc
+	return w, train_acc, test_acc
 
 

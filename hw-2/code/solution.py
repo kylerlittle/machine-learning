5cdaf3a68f414e0f89a6970e4928db985cbd7dbc
kylerlittle <kyler.little@wsu.edu>
2018-02-03 19:00:09 -0800

    log regression implement

--- hw-2/code/solution.py
+++ hw-2/code/solution.py
@@ -4,25 +4,40 @@ from helper import *
 Homework2: logistic regression classifier
 '''
 
+'''
+Auxilary Functions
+'''
+def sigmoid(x):
+        return 1./(1.+np.exp(-x))
+
+def linear_signal(w, x):
+        return np.dot(x,w)
+
+def compute_grad_cross_entropy(x, y, w):
+        grad_cross_entropy = np.zeros((x.shape[1], 1))
+        for i in np.arange(len(x)):
+                np.transpose(grad_cross_entropy)[0] += (y[i] * x[i]) / (1+np.exp(y[i] * linear_signal(w,x[i]))[0])
+        grad_cross_entropy = -1./len(x) * grad_cross_entropy
+        return grad_cross_entropy
 
 def logistic_regression(data, label, max_iter, learning_rate):
-        w = np.zeros((data.shape[1], 1))
-        pass
-
+        # Initialize variables.
+        w = np.zeros((data.shape[1], 1)); allowableError = 1.0e-6; t = 0
+        gradient_cross_entropy = np.ones((data.shape[1], 1))   # initialize 'gradient_cross_entropy' to something nonzero
+        # Run the algorithm
+        while t < max_iter and abs(np.linalg.norm(gradient_cross_entropy)) > allowableError:
+                gradient_cross_entropy = compute_grad_cross_entropy(data, label, w)
+                w = w - (learning_rate * gradient_cross_entropy)
+                t +=1
+        return w
 
 def thirdorder(data):
 	pass
 
-
-def sigmoid(x):
-        return 1./(1.+np.exp(-x))
-
 def accuracy(x, y, w):
         threshold = 0.5; correctlyClassified = 0
-        for index in range(x):
-                predictedVal = 1. if (sigmoid(np.dot(np.transpose(w),x[index]))) > threshold else -1.
+        for index in range(len(x)):
+                predictedVal = 1. if (sigmoid(linear_signal(w,x[index]))) > threshold else -1.
                 if predictedVal == y[index]:
                         correctlyClassified += 1
         return float(correctlyClassified) / len(x)
-
-

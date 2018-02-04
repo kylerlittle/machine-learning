8137c59e0ff72ee52c9272bada98858a06811478
kylerlittle <kyler.little@wsu.edu>
2018-02-04 11:29:32 -0800

    coding part done

--- hw-2/code/solution.py
+++ hw-2/code/solution.py
@@ -8,12 +8,21 @@ Homework2: logistic regression classifier
 Auxilary Functions
 '''
 def sigmoid(x):
-        return 1./(1.+np.exp(-x))
+        '''
+        The probabilistic function (a standard sigmoid curve)
+        '''
+        return np.exp(x)/(1.+np.exp(x))
 
 def linear_signal(w, x):
+        '''
+        Calculates the linear signal (i.e. the dot product between w and x)
+        '''
         return np.dot(x,w)
 
 def compute_grad_cross_entropy(x, y, w):
+        '''
+        Computes the gradient of the cross entropy function 
+        '''
         grad_cross_entropy = np.zeros((x.shape[1], 1))
         for i in np.arange(len(x)):
                 np.transpose(grad_cross_entropy)[0] += (y[i] * x[i]) / (1+np.exp(y[i] * linear_signal(w,x[i]))[0])
@@ -21,6 +30,10 @@ def compute_grad_cross_entropy(x, y, w):
         return grad_cross_entropy
 
 def compute_transformation(x):
+        '''
+        Calculates the third-order polynomial transform of 2-dimensional input vector [1.0, x1, x2]
+        In this case, we've passed in x as just [x1, x2], so I adjust indices accordingly
+        '''
         third_transform = np.zeros(10)
         third_transform[0] = 1.0; third_transform[1] = x[0]
         third_transform[2] = x[1]; third_transform[3] = (x[0])**2.0
@@ -30,6 +43,17 @@ def compute_transformation(x):
         return third_transform
 
 def logistic_regression(data, label, max_iter, learning_rate):
+        '''
+        The logistic regression classifier function. 
+        Args:
+        data: train data with shape (1561, 3), which means 1561 samples and each sample has 3 features.(1, symmetry, average internsity)
+        label: train data's label with shape (1561,1). 1 for digit number 1 and -1 for digit number 5.
+        max_iter: max iteration numbers
+        learning_rate: learning rate for weight update
+        
+        Returns:
+        w: the seperater with shape (3, 1). You must initilize it with w = np.zeros((d,1))
+        '''
         # Initialize variables.
         w = np.zeros((data.shape[1], 1)); allowableError = 1.0e-6; t = 0
         gradient_cross_entropy = np.ones((data.shape[1], 1))   # initialize 'gradient_cross_entropy' to something nonzero
@@ -42,12 +66,36 @@ def logistic_regression(data, label, max_iter, learning_rate):
         return w
 
 def thirdorder(data):
-        transformed_data = np.zeros((len(data), 10))
-        for index in np.arange(len(data)):
-                transformed_data[index] = compute_transformation(data[index])
-        return transformed_data
+	'''
+	This function is used for a 3rd order polynomial transform of the data.
+	
+	Args:
+	data: input data with shape (:, 3) the first dimension represents total samples (training: 1561; testing: 424) and the second dimesion represents total features.
+	
+	Return:
+	result: A numpy array format new data with shape (:,10), which using a 3rd order polynomial transformation to extend the feature numbers from 3 to 10. 
+	The first dimension represents total samples (training: 1561; testing: 424) and the second dimesion represents total features
+	'''
+	transformed_data = np.zeros((len(data), 10))
+
+	for index in np.arange(len(data)):
+	    transformed_data[index] = compute_transformation(data[index])
+	return transformed_data
+	    
+
 
 def accuracy(x, y, w):
+        '''
+        This function is used to compute accuracy of a logsitic regression model.
+        
+        Args:
+        x: input data with shape (n, d), where n represents total data samples and d represents total feature numbers of a certain data sample.
+        y: corresponding label of x with shape(n, 1), where n represents total data samples.
+        w: the seperator learnt from logistic regression function with shape (d, 1), where d represents total feature numbers of a certain data sample.
+        
+        Return 
+        accuracy: total percents of correctly classified samples. Set the threshold as 0.5, which means, if the predicted probability > 0.5, classify as 1; Otherwise, classify as -1.
+        '''
         threshold = 0.5; correctlyClassified = 0
         for index in range(len(x)):
                 predictedVal = 1. if (sigmoid(linear_signal(w,x[index]))) > threshold else -1.

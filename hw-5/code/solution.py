774606af3ac8cabc4fea6113362154aa51b39d35
kylerlittle <kyler.little@wsu.edu>
2018-04-15 13:02:07 -0700

    assignment complete

--- hw-5/code/solution.py
+++ hw-5/code/solution.py
@@ -12,23 +12,29 @@ two functions.
 '''
 
 def svm_with_diff_c(train_label, train_data, test_label, test_data):
-	'''
-	Use 'svm_train' function with training label, data and different value 
-	of cost c to train a svm classify model. Then apply the trained model
-	on testing label and data.
-	
-	The value of cost c you need to try is listing as follow:
-	c = [0.01, 0.1, 1, 2, 3, 5]
-	Please keep other parameter options as default.
-	No return value is needed
-	'''
+        '''
+        Use 'svm_train' function with training label, data and different value 
+        of cost c to train a svm classify model. Then apply the trained model
+        on testing label and data.
+        
+        The value of cost c you need to try is listing as follow:
+        c = [0.01, 0.1, 1, 2, 3, 5]
+        Please keep other parameter options as default.
+        No return value is needed
+        '''
+        diff_costs = [0.01, 0.1, 1, 2, 3, 5]
+        for cost in diff_costs:
+                print("\nCost:", cost)
+                libsvm_options = '-c ' + str(cost)
+                model = svm_train(train_label, train_data, libsvm_options)
+                predicted_label, test_acc, decision_values = svm_predict(test_label, test_data, model)
 
 def svm_with_diff_kernel(train_label, train_data, test_label, test_data):
-	'''
+        '''
 	Use 'svm_train' function with training label, data and different kernel
-	to train a svm classify model. Then apply the trained model
+        to train a svm classify model. Then apply the trained model
 	on testing label and data.
-	
+
 	The kernel  you need to try is listing as follow:
 	1. linear kernel
 	2. polynomial kernel
@@ -36,3 +42,9 @@ def svm_with_diff_kernel(train_label, train_data, test_label, test_data):
 	Please keep other parameter options as default.
 	No return value is needed
 	'''
+        diff_kernels = [0,1,2]
+        for k in diff_kernels:
+                print("\nKernel Option:", k)
+                libsvm_options = '-t ' + str(k)
+                model = svm_train(train_label, train_data, libsvm_options)
+                predicted_label, test_acc, decision_values = svm_predict(test_label, test_data, model)
